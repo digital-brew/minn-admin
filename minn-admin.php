540281cd1962cfbd61b8d1a0a41aa94e3f93d11a
Austin Ginder <austinginder@gmail.com>
2026-07-04 09:47:35 -0400

    📦 NEW: AnalyticsWP traffic adapter

--- minn-admin.php
+++ minn-admin.php
@@ -33,6 +33,7 @@ require_once MINN_ADMIN_DIR . 'includes/adapters/koko-analytics.php';
 require_once MINN_ADMIN_DIR . 'includes/adapters/wp-statistics.php';
 require_once MINN_ADMIN_DIR . 'includes/adapters/burst-statistics.php';
 require_once MINN_ADMIN_DIR . 'includes/adapters/independent-analytics.php';
+require_once MINN_ADMIN_DIR . 'includes/adapters/analyticswp.php';
 require_once MINN_ADMIN_DIR . 'includes/adapters/simple-history.php';
 require_once MINN_ADMIN_DIR . 'includes/adapters/redirection.php';
 
